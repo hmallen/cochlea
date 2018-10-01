1120270ebd98a75116412490940462f6b3f3ba99
hmallen <allenhm@gmail.com>
2018-10-01 01:27:41 -0500

    Testing lcd functions.

--- testing/test_i2c_lcd.py
+++ testing/test_i2c_lcd.py
@@ -5,6 +5,8 @@ import time
 if __name__ == '__main__':
     lcd_screen = i2c_lcd_driver.lcd()
 
+    lcd_screen.backlight(1)
+
     lcd_screen.lcd_display_string('Hello, world!', 1)
 
     str_pad = ' ' * 16
