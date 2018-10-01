c02794f5e04fe651568047a15fa3f057f88e240e
hmallen <allenhm@gmail.com>
2018-10-01 09:26:15 -0500

    Cleaned-up code.

--- morse_keyer.py
+++ morse_keyer.py
@@ -17,13 +17,12 @@ with open(morse_reference_file) as file:
 
 class MorseKeyer:
 
-    #def __init__(self, bcm_pin, dot_delay=0.15, dash_delay=0.45, interkey_delay=0.15, interword_delay=1.05, angle_rest=0, angle_keyed=15, travel_delay=0.1):
-    def __init__(self, bcm_pin, dot_delay=0.05, angle_rest=0, angle_keyed=15): #, travel_delay=0.1):
+    def __init__(self, bcm_pin, dot_delay=0.075, angle_rest=0, angle_keyed=15):
         self.dot_delay = dot_delay
-        self.dash_delay = dot_delay * 3 #dash_delay
+        self.dash_delay = dot_delay * 3
 
-        self.interkey_delay = dot_delay #interkey_delay
-        self.interword_delay = dot_delay * 7 #interword_delay
+        self.interkey_delay = dot_delay
+        self.interword_delay = dot_delay * 7
 
         self.angle_rest = angle_rest
         self.angle_keyed = angle_keyed
