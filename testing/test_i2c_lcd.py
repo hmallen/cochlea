68e43bf34a48b67bd72d2d9ec38939160f91c548
hmallen <allenhm@gmail.com>
2018-10-01 00:36:01 -0500

    Added infinite scroll test.

--- testing/test_i2c_lcd.py
+++ testing/test_i2c_lcd.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
     str_pad = ' ' * 16
     display_string = str_pad + 'Hello, world! Making longer to scroll.'
 
-    for x in range(0, len(display_string)):
-        display_text = display_string[x:(x+16)]
-        lcd_screen.lcd_display_string(display_text, 1)
-        time.sleep(0.1)
-        lcd_screen.lcd_display_string(str_pad, 1)
+    while True:
+        for x in range(0, len(display_string)):
+            display_text = display_string[x:(x+16)]
+            lcd_screen.lcd_display_string(display_text, 1)
+            time.sleep(0.1)
+            lcd_screen.lcd_display_string(str_pad, 1)
