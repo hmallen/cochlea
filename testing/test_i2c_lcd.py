1afa257f70a9cc0226b2fe6ea0eab786b12ff039
hmallen <allenhm@gmail.com>
2018-10-01 01:23:51 -0500

    Testing scroll.

--- testing/test_i2c_lcd.py
+++ testing/test_i2c_lcd.py
@@ -10,9 +10,19 @@ if __name__ == '__main__':
     str_pad = ' ' * 16
     display_string = str_pad + 'Hello, world! Making longer to scroll.' + str_pad
 
-    while True:
-        for x in range(0, (len(display_string) - 16)):
-            display_text = display_string[x:(x+16)]
-            lcd_screen.lcd_display_string(display_text, 2)
-            time.sleep(0.1)
-            # lcd_screen.lcd_display_string(str_pad, 2)
+    for x in range(0, (len(display_string) - 16)):
+        display_text = display_string[x:(x+16)]
+        lcd_screen.lcd_display_string(display_text, 2)
+        time.sleep(0.1)
+        # lcd_screen.lcd_display_string(str_pad, 2)
+
+    lcd_screen.clear()
+    time.sleep(1)
+
+    display_string_short = display_string[16:]
+    lcd_screen.lcd_display_string(display_string_short, 1)
+    time.sleep(2.5)
+    for x in range(0, (len(display_string) - 16)):
+        display_text = display_string_short[x:(x+16)]
+        lcd_screen.lcd_display_string(display_text, 1)
+        time.sleep(0.1)
