bf83c07badf1e841c4e26960ad06d8ca0ee2f893
hmallen <allenhm@gmail.com>
2018-10-01 09:37:14 -0500

    Changed defaults.

--- morse_keyer.py
+++ morse_keyer.py
@@ -27,7 +27,7 @@ class MorseKeyer:
         self.angle_rest = angle_rest
         self.angle_keyed = angle_keyed
 
-        self.travel_delay = dot_delay / 2
+        self.travel_delay = dot_delay / 1.5
 
         self.servo = AngularServo(bcm_pin)
         self.servo.angle = self.angle_rest
