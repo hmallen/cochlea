6b4e00216888aa76caa6fa42fe0266f2f650c9ff
hmallen <allenhm@gmail.com>
2018-10-01 00:34:06 -0500

    Added lcd string scroll test.

--- testing/test_i2c_lcd.py
+++ testing/test_i2c_lcd.py
@@ -1,7 +1,15 @@
 import i2c_lcd_driver
+import time
 
 
 if __name__ == '__main__':
     lcd_screen = i2c_lcd_driver.lcd()
 
-    lcd_screen.lcd_display_string('Hello, world!', 1)
+    str_pad = ' ' * 16
+    display_string = str_pad + 'Hello, world! Making longer to scroll.'
+
+    for x in range(0, len(display_string)):
+        display_text = display_string[x:(x+16)]
+        lcd_screen.lcd_display_string(display_text, 1)
+        time.sleep(0.4)
+        lcd_screen.lcd_display_string(str_pad, 1)
